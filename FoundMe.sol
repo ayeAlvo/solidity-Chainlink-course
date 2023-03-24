d74843a916ac51c716aba5813d4874858404ddf9
ayeAlvo <ayealvo@gmail.com>
2023-03-24 20:57:26 -0300

    feat(FoundMe): new functions

--- FoundMe.sol
+++ FoundMe.sol
@@ -1,14 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.9.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract FundMe {
 
+    // safe math library check uint256 for integer overflows
+    using SafeMathChainlink for uint256;
+
     mapping(address => uint256) public addressToAmountFunded;
 
+    address[] public funders;
+
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
+        uint256 minimumUSD = 20 * 10 ** 18;
+        require(getConversionRate(msg.value) >= minimumUSD, "Errror: You need to spend more eth!");
         addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
 
     function getVersion() public view returns(uint256) {
@@ -35,5 +50,20 @@ contract FundMe {
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * _ethAmount) / 1000000000000000000;
         return ethAmountInUsd;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    function withdraw() payable onlyOwner public {
+        msg.sender.transfer(address(this).balance);
+        for(uint256 funderIndex; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
     }
 }
